21a64766c6a78091e2ea0a4d5bb361a97e643407
RAGHUVEER SINGH <53135497+Raghuveersingh-2097@users.noreply.github.com>
2021-01-12 21:31:03 +0530

    Add files via upload

---
+++ Inheritance/com.cg.lab4.ques3/domain/MediaItem.java
@@ -0,0 +1,12 @@
+package com.cg.items.domain;
+
+public abstract class MediaItem{
+private int runTime;
+public int getRunTime() {
+	return runTime;
+}
+public void setRunTime(int runTime) {
+	this.runTime = runTime;
+}
+}
+
