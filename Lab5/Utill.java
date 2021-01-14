3b260800d6a418c35ebbc8013979e193c7b65b34
RAGHUVEER SINGH <53135497+Raghuveersingh-2097@users.noreply.github.com>
2021-01-14 20:23:29 +0530

    Update Utill.java

--- Lab5/Utill.java
+++ Lab5/Utill.java
@@ -6,30 +6,33 @@ import com.cg.exception_lab.customexception.*;
 import com.cg.exception_lab.domain.ExceptionInfo;
 import com.cg.exception_lab.exception.*;
 public class Utill {
+/**
+*--------------------------------------------Utility class for the validation of the person-----------------------------------------------------*
+**/	
 	 public static void mains() {
 		EmployeeService employeeservice=new EmployeeServiceImpl();
 		ExceptionInfo inf=new ExceptionInfo();
 		try {
-			Scanner sc=new Scanner(System.in);
-		int age=sc.nextInt();
-			employeeservice.age_validation(inf.setAge(age));
-		String name=sc.next();
-		String surname=sc.next();
-			employeeservice.name_validation(inf.setName(name),inf.setSurname(surname));
-			double salary=sc.nextInt();
-			employeeservice.salary_validation(inf.setSalary(salary));	
-		}
-		catch(InvalidAgeException e) {
-			System.out.println(e.getMessage());
-			System.exit(0);
-		}
-		catch(InvalidNameException e) {
-			System.out.println(e.getMessage());
-			System.exit(0);
-		}
-		catch(InvalidSalaryException e) {
-			System.out.println(e.getMessage());
-			System.exit(0);
-		}
-	}
+	              Scanner sc=new Scanner(System.in);
+		      int age=sc.nextInt();
+		                       employeeservice.age_validation(inf.setAge(age));
+		      String name=sc.next();
+		      String surname=sc.next();
+			               employeeservice.name_validation(inf.setName(name),inf.setSurname(surname));
+		      double salary=sc.nextInt();
+			               employeeservice.salary_validation(inf.setSalary(salary));	
+}
+		            catch(InvalidAgeException e) {
+			      System.out.println(e.getMessage());
+			      System.exit(0);
+}
+		           catch(InvalidNameException e) {
+			     System.out.println(e.getMessage());
+			     System.exit(0);
+}
+		           catch(InvalidSalaryException e) {
+			     System.out.println(e.getMessage());
+			     System.exit(0);
+}
+}
 }
