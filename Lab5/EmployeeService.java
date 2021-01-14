f8cb256c051c5e5a01c2913ad1b4158041877f35
RAGHUVEER SINGH <53135497+Raghuveersingh-2097@users.noreply.github.com>
2021-01-14 20:11:17 +0530

    Update EmployeeService.java

--- Lab5/EmployeeService.java
+++ Lab5/EmployeeService.java
@@ -3,7 +3,16 @@ package com.cg.exception_lab.customexception;
 import com.cg.exception_lab.exception.*;
 
 public interface EmployeeService {
+/**
+*-----------------------------------------Check the validity of age---------------------------------------*
+**/	
 	void age_validation(int age ) throws InvalidAgeException; 
+/**
+*-----------------------------------------Check the validity of name---------------------------------------*
+**/
     void name_validation(String name,String surname ) throws InvalidNameException;
+/**
+*-----------------------------------------Check the validity of salary---------------------------------------*
+**/
     void salary_validation(double salary)throws InvalidSalaryException;
 }
