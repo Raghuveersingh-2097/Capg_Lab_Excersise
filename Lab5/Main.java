943b6b652f98fbc163579496fee8dafc93d2dc58
RAGHUVEER SINGH <53135497+Raghuveersingh-2097@users.noreply.github.com>
2021-01-14 20:19:28 +0530

    Update Main.java

--- Lab5/Main.java
+++ Lab5/Main.java
@@ -1,14 +1,13 @@
 package com.cg.assigment.main;
 
 import com.cg.assigment.util.Utill;
-
+/**
+*-----------------------------------------------------------Main-Class--------------------------------------------------------------------------*
+**/
 public class Main {
 
 	public static void main(String[] args) {
-		Utill util=new Utill();
-		util.mains();
-		
-
-	}
-
+	Utill util=new Utill();
+	util.mains();		
+}
 }
